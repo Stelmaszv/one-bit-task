5993d4c1fbe21a9190e0ec860d3e846c181788fc
= <stelmaszv@gmail.com>
2022-07-26 13:42:28 +0200

    add Currency table

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpClient\HttpClient;
 use App\Forms\Form;
 use Symfony\Component\Form\Form as SymfonyForm;
 
@@ -26,20 +27,45 @@ class MainController extends AbstractController
         }
     }
 
-    private function no_submit(SymfonyForm $form) : Response{
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-            'form'=> $form->createView(),
-            'result'=>'d'
+    private function no_submit(SymfonyForm $form) : Response
+    {
+        return $this->render('main/nosubmit.html.twig', [
+            'form'=> $form->createView()
         ]);
     }
 
-    private function submit(SymfonyForm $form) : Response{
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-            'form'=> $form->createView(),
-            'result'=>'F'
+    private function submit(SymfonyForm $form) : Response
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://api.frankfurter.app/2022-01-11?base=PLN&symbols=EUR,USD,GBP,CZK');
+        $data=$this->set_data($response->toArray());
+        return $this->render('main/submit.html.twig', [
+            'results'=>$data
         ]);
+    }
+
+    private function set_data(array $data) :array
+    {
+        $return_data=[];
+        foreach($data['rates'] as $rate => $key){
+            $get_today_rate=$this->get_today_rate($key);
+            $return_data[] = array(
+                "Currency"          => $rate,
+                "Rate_from_today"   => $get_today_rate,
+                "Indicated_date"    => $key,
+                "Percentage_change" => $this->get_percentage_change($get_today_rate,$key)
+            );
+        }
+        return $return_data;
+    }
+    private function get_percentage_change(float $today_rate,float $rate):int
+    {
+        return 1;
+    }
+
+    private function get_today_rate(string $key) :float
+    {
+        return -123456.789;
     }
 
 
